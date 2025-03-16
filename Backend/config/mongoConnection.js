4c3eb069d879fe234e592b69a3fd40ac331faad5
Celocious Altrin <celociousalltrinmf@gmail.com>
2025-03-16 12:14:02 +0530

    Code updated

--- Backend/config/mongoConnection.js
+++ Backend/config/mongoConnection.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  console.log(
-    "🚀 ~ connectDB ~ process.env.DATABASE_URI:",
-    process.env.DATABASE_URI
-  );
   try {
     await mongoose.connect(process.env.DATABASE_URI, {
       useNewUrlParser: true,
