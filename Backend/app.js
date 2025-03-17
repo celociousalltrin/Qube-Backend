d4d5b81970c997d5b83fff8dd2a866902ee7914a
Celocious Altrin <celociousalltrinmf@gmail.com>
2025-03-17 07:16:49 +0530

    Code updated

--- Backend/app.js
+++ Backend/app.js
@@ -19,13 +19,6 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 app.use(cookieParser());
 
-// app.use(
-//   cors({
-//     origin: process.env.REACT_APP_URL,
-//     credentials: true,
-//   })
-// );
-
 app.use(
   cors({
     origin: true, // Accepts any origin (any domain, protocol, or port)
